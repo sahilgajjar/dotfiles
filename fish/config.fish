0e1e7d452ba5d1a5c5b29251f818779a5ac29fe1
sahil_gajjar <sahilgajjar504@gmail.com>
2022-12-13 21:35:14 +0530

    added new fish config folder

---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# ~/.config/fish/config.fish
+
+alias nvim='~/./nvim.appimage'
+alias launch='~/.local/bin/./pipes.sh'
+
+
+#source ~/.local/share/icons-in-terminal/icons.fish
+
+#pop os logo
+$HOME/.pokemon-icat/pokemon-icat.sh
+
+
+
